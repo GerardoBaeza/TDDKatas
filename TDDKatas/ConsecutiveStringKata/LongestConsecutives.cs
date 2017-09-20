cb2a3e04c81feaa2ea0951878ca9c53567ec6e95
Gerardo Baeza Cervantes <gbaeza@syseti.com>
2017-09-20 16:25:48 -0600

    git init

---
+++ TDDKatas/ConsecutiveStringKata/LongestConsecutives.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDKatas.ConsecutiveStringKata
+{
+    public class LongestConsecutives
+    {
+        public string LongestConsec(string[] strarr, int k)
+        {
+            if (strarr.Length != 0 && k<strarr.Length)
+            {
+                int maxLenght = 0;
+                int startPoint = 0;
+                string finalString = "";
+                for (int i = 0; i <= strarr.Length - k; i++)
+                {
+                    int temporalLenght = 0;
+                    for (int j = 0; j <= k - 1; j++)
+                    {
+                        temporalLenght += strarr[j + i].Length;
+                    }
+                    if (temporalLenght > maxLenght)
+                    {
+                        maxLenght = temporalLenght;
+                        startPoint = i;
+                    }
+                }
+                if (k != 1)
+                {
+                    var startPointPivot = startPoint;
+                    while (startPointPivot<startPoint+k)
+                    { 
+                        finalString += strarr[startPointPivot];
+                        startPointPivot++;
+                    }
+                }
+                else
+                    finalString = strarr[startPoint];
+                return finalString;
+            }
+            else return "";
+        }
+    }
+}
---
+++ TDDKatasTest/ConsecutiveStringKataTest/LongestConsecutivesTests .cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using TDDKatas.ConsecutiveStringKata;
+
+
+namespace TDDKatasTest.ConsecutiveStringKataTest
+{
+    using NUnit.Framework;
+    [TestFixture]
+    public class LongestConsecutivesTests
+    {
+        LongestConsecutives longest= new LongestConsecutives();
+        [Test]
+        public  void LongestConsecutivestest1()
+        {
+            var actual= longest.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2);
+            Assert.AreEqual("abigailtheta", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest2()
+        {
+            var actual = longest.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1);
+            Assert.AreEqual("oocccffuucccjjjkkkjyyyeehh", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest3()
+        {
+            var actual = longest.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2);
+            Assert.AreEqual("wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest4()
+        {
+            var actual = longest.LongestConsec(new String[] { }, 3);
+            Assert.AreEqual("", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest5()
+        {
+            var actual = longest.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2);
+            Assert.AreEqual("wlwsasphmxxowiaxujylentrklctozmymu", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest6()
+        {
+            var actual = longest.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2) ;
+            Assert.AreEqual("", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest7()
+        {
+            var actual = longest.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3);
+            Assert.AreEqual("ixoyx3452zzzzzzzzzzzz", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest8()
+        {
+            var actual = longest.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15) ;
+            Assert.AreEqual("", actual);
+        }
+        [Test]
+        public  void LongestConsecutivestest9()
+        {
+            var actual=longest.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0);
+            Assert.AreEqual("", actual);
+        }
+
+    }
+}
